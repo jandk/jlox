090f07b1668c185fb7a3ef763428bec8653fb854
Jan De Kock <tjoener@gmail.com>
2017-10-31 19:39:35 +0100

    Chapter 4.4 - The Scanner Class

--- src/main/kotlin/be/tjoener/jlox/parser/Scanner.kt
+++ src/main/kotlin/be/tjoener/jlox/parser/Scanner.kt
@@ -1,7 +1,32 @@
 package be.tjoener.jlox.parser
 
+import be.tjoener.jlox.parser.TokenType.EOF
+
 class Scanner(val source: String) {
+
+    private val tokens = mutableListOf<Token>()
+
+    private var start: Int = 0
+    private var current: Int = 0
+    private var line: Int = 1
+
     fun scanTokens(): List<Token> {
-        return listOf()
+        while (!isAtEnd()) {
+            // We are at the beginning of the next lexeme.
+            start = current
+            scanToken()
+        }
+
+        tokens.add(Token(EOF, "", null, line))
+        return tokens
     }
+
+    private fun scanToken() {
+        TODO()
+    }
+
+    private fun isAtEnd(): Boolean {
+        return current >= source.length
+    }
+
 }
