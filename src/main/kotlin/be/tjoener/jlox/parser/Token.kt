3150638094998bdfab80ad553fcedf67673d2b4d
Jan De Kock <tjoener@gmail.com>
2017-10-31 19:30:19 +0100

    Chapter 4.2 - Tokens and Lexemes

--- src/main/kotlin/be/tjoener/jlox/parser/Token.kt
+++ src/main/kotlin/be/tjoener/jlox/parser/Token.kt
@@ -1,4 +1,9 @@
 package be.tjoener.jlox.parser
 
-class Token {
+class Token(val type: TokenType, val lexeme: String, val literal: Any?, val line: Int) {
+
+    override fun toString(): String {
+        return "$type $lexeme $literal"
+    }
+
 }
