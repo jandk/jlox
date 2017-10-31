ef8cdf2bddccc645202b712aee13cbb814b228af
Jan De Kock <tjoener@gmail.com>
2017-10-31 21:16:27 +0100

    Chapter 6.2 - Recursive Descent Parsing

---
+++ src/main/kotlin/be/tjoener/jlox/parser/Parser.kt
@@ -0,0 +1,122 @@
+package be.tjoener.jlox.parser
+
+import be.tjoener.jlox.ast.*
+import be.tjoener.jlox.parser.TokenType.*
+
+
+class Parser(val tokens: List<Token>) {
+
+    private var current = 0
+
+    private fun expression(): Expr {
+        return equality()
+    }
+
+    private fun equality(): Expr {
+        var expr = comparison()
+
+        while (match(EQUAL_EQUAL, BANG_EQUAL)) {
+            val operator = previous()
+            val right = comparison()
+            expr = Binary(expr, operator, right)
+        }
+
+        return expr
+    }
+
+    private fun comparison(): Expr {
+        var expr = addition()
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            val operator = previous()
+            val right = addition()
+            expr = Binary(expr, operator, right)
+        }
+
+        return expr
+    }
+
+    private fun addition(): Expr {
+        var expr = multiplication()
+
+        while (match(PLUS, MINUS)) {
+            val operator = previous()
+            val right = multiplication()
+            expr = Binary(expr, operator, right)
+        }
+
+        return expr
+    }
+
+    private fun multiplication(): Expr {
+        var expr = unary()
+
+        while (match(SLASH, STAR)) {
+            val operator = previous()
+            val right = unary()
+            expr = Binary(expr, operator, right)
+        }
+
+        return expr
+    }
+
+    private fun unary(): Expr {
+        if (match(BANG, MINUS)) {
+            val operator = previous()
+            val right = unary()
+            return Unary(operator, right)
+        }
+
+        return primary()
+    }
+
+    private fun primary(): Expr {
+        if (match(FALSE)) return Literal(false)
+        if (match(TRUE)) return Literal(true)
+        if (match(NIL)) return Literal(null)
+
+        if (match(NUMBER, STRING)) {
+            return Literal(previous().literal)
+        }
+
+        if (match(LEFT_PAREN)) {
+            val expr = expression()
+            consume(RIGHT_PAREN, "Expect ')' after expression")
+            return Grouping(expr)
+        }
+
+        TODO()
+    }
+
+    private fun consume(type: TokenType, message: String) {
+        TODO()
+    }
+
+    private fun match(vararg types: TokenType): Boolean {
+        val matches = types.any { check(it) }
+        if (matches) advance()
+        return matches
+    }
+
+    private fun check(tokenType: TokenType): Boolean {
+        return if (isAtEnd()) false else peek().type === tokenType
+    }
+
+    private fun advance(): Token {
+        if (!isAtEnd()) current++
+        return previous()
+    }
+
+    private fun isAtEnd(): Boolean {
+        return peek().type === EOF
+    }
+
+    private fun peek(): Token {
+        return tokens[current]
+    }
+
+    private fun previous(): Token {
+        return tokens[current - 1]
+    }
+
+}
